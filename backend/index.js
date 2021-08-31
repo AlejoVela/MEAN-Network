c46157e08df28a3e6973b2d180b4da30e42b5df9
Diego R <alejorvela@gmail.com>
2021-08-31 11:55:16 -0500

    add server and local port

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 const { dbConnection } = require('./db/db');
 const User = require('./routes/user');
+const Post = require('./routes/post');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/user', User);
+app.use('/api/post', Post);
 
-app.listen(process.env.PORT, console.log(`Server is listen OK on port ${process.env.PORT}`))
+app.listen(process.env.PORT || 3000, console.log(`Server is listen OK on port ${process.env.PORT}`))
 
 dbConnection();
